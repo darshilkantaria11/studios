a26e1abcb07aea57aceb98013c0e0e2eee5d88ad
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-08-30 22:05:26 +0530

    google ads tag added

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,6 @@ import Navbar from "./nopage/component/navbar"
 import Footer from "./nopage/component/footer";
 import Script from 'next/script';
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,29 +18,32 @@ export const metadata = {
   metadataBase: new URL("https://designuix.com"),
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C0T3395KGL"></Script>
-      <Script id="google-analytic">
-        {
-          `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-    
-            gtag('config', 'G-C0T3395KGL');
-              `
-        }
+      {/* Google Analytics (GA4) */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C0T3395KGL"></Script>
 
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          
+          gtag('config', 'G-C0T3395KGL');
+
+          
+          gtag('config', 'AW-17274749876');
+        `}
       </Script>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
